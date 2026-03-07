b8ff8222a2c03c95b7aae65d8d02fe5263948555
tejaadusumilli21 <teamteja3@gmail.com>
2026-03-07 13:27:23 +0530

    feat: configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ⚠️ Change 'lumina-auth' to match YOUR GitHub repository name exactly
 export default defineConfig({
   plugins: [react()],
-  base: '/teja_dashboard/',
+  base: '/Plio/',
 })
